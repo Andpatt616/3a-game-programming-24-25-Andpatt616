6f83fcfd3e4c51cd5a72619c9f826646ff079c6a
Andpatt616 <s12421244@students.duvalschools.org>
2024-08-14 12:57:16 -0400

    v0.2
    We used \n characters,

--- 00_computer_science_exercises/00_helloWorld/hello_world.py
+++ 00_computer_science_exercises/00_helloWorld/hello_world.py
@@ -1,3 +1,9 @@
 # Hello World, Andrew Patton, v0.1
 
 print() # print() is a built-in FUNCTION for Python.
+
+# SYNTAX Erroes -- error with the way code is written, such as missing characters or incorrect case
+print("Hello World")
+print ("New Line Character\n") # \n adds an empty line after printing
+
+print("How many\n lines will\n this print on\n the screen?\n")
