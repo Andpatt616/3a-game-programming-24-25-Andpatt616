08b4c04ac5b3dd1af551a7f1de0d5a36f6d1fe35
Andpatt616 <s12421244@students.duvalschools.org>
2024-08-14 13:21:34 -0400

    data

---
+++ 00_computer_science_exercises/01_dataTypes_Operators/data_types_operators.py
@@ -0,0 +1,19 @@
+# Data Types and Operators, Andrew Patton, v0.0
+
+# Fundamental Data Types
+# String - str - Sequence of letter, numbers, spaces, or other characters.
+# Strings in Python are inside ' ' or " "
+# idc if you use ' ' or " " just be consistent.
+
+# Boolean - bool - True or False values.
+
+# Float - float - any number with a decimal, +/-, including 0.0.
+
+# Integers - int - any whole numbers, +/-, including 0.
+
+# Data types are stored in VARIABLES.
+# A variable is basically a bucket with a name to put data into.
+# VARIABLE NAMES SHOULD TELL YOU WHAT DATA IS BEING STORED IN IT.
+# VARIABLES CANNOT START WITH A NUMBER
+# camelCaseVariableNames
+# snake_case_variable_names
